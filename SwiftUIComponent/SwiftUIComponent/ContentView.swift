be178139e2f3cb460856832eb51c1fa26cef840f
Carlos pava <carlos.pava@overactive.com>
2022-06-28 11:25:30 -0500

    add project.

---
+++ SwiftUIComponent/SwiftUIComponent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIComponent
+//
+//  Created by Carlos Pava on 28/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIComponent/SwiftUIComponent/SwiftUIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentApp.swift
+//  SwiftUIComponent
+//
+//  Created by Carlos Pava on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
