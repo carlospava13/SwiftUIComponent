6e3de17554d2e07cade770418e401cf6e63b6296
Carlos pava <carlos.pava@overactive.com>
2022-06-28 11:35:17 -0500

    add colors.

--- SwiftUIComponent/SwiftUIComponent/HomeView.swift
+++ SwiftUIComponent/SwiftUIComponent/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  SwiftUIComponent
 //
 //  Created by Carlos Pava on 28/06/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
--- SwiftUIComponent/SwiftUIComponent/SwiftUIComponentApp.swift
+++ SwiftUIComponent/SwiftUIComponent/SwiftUIComponentApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIComponentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
